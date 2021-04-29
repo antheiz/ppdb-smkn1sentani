10f924b4c7cbd514e2fd9ead2fc8be4cb165773a
antroytheiz <theisandatu@gmail.com>
2021-04-29 12:57:28 +0900

    ubah valdiasi nama lengkap

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationE
 from .models import AkunPengguna
 
 class DaftarAkunForm(FlaskForm):
-    nama_lengkap = StringField('nama_lengkap', validators=[DataRequired(), Length(max=40, message='Nama lengkap tidak sesuai, Silahkan periksa kembali')])
+    nama_lengkap = StringField('nama_lengkap', validators=[DataRequired(), Length(max=25, message='Nama lengkap tidak sesuai, Silahkan periksa kembali')])
     email = StringField('email', validators=[DataRequired(), Email('Email salah, Silahkan periksa kembali')])
     kata_sandi = PasswordField('kata_sandi', validators=[DataRequired(), Length(min=8, message='Kata sandi minimal 8 Karakter, Silahkan periksa kembali') ])
     ulangi_katasandi = PasswordField('ulangi_katasandi', validators=[DataRequired(), EqualTo('kata_sandi', message='Kata sandi salah, Silahkan periksa kembali'), Length(min=8, message='Kata sandi minimal 8 Karakter, Silahkan periksa kembali')])
--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ def load_user(user_id):
 
 class AkunPengguna(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    nama_lengkap = db.Column(db.String(40), nullable=False)
-    email = db.Column(db.String(30), unique=True, nullable=False)
+    nama_lengkap = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(25), unique=True, nullable=False)
     foto_profil = db.Column(db.String(30), nullable=False, default='defaults.png')
     kata_sandi = db.Column(db.Text, nullable=False)
